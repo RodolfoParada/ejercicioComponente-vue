b3b390e3e4448c11c2ecf26af1fc37fbf9858c79
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-06-06 00:16:44 -0400

    correciones en la tabla y eliminacion de elementos que no se utilizan

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const text = {
         <tr>
             <th class = "text-bold" style="color:white"> {{ fecha }} </th>  
         </tr>
-
+     </table>  
   `
 }
 
@@ -34,36 +34,6 @@ createApp({
     },
     data() {
         return {
-
-            tituloIndex: "Hola desde index",
-            titulo: "Pizza Napoletana",
-            portada: "https://static.cookist.it/wp-content/uploads/sites/21/2017/12/istock-480277738.jpg",
-            costo: 675.00,
-            codermeals: [{
-                    id: 1,
-                    titulo: "Spaghetti alla puttanesca",
-                    costo: 575.00,
-                    portada: "https://4.bp.blogspot.com/-D5Wvi_gX_Kg/WLatk_GVnKI/AAAAAAAAA5M/-y0gB26R0Dkb01QmdgfZqQALtd9NMV2DACLcB/s1600/P70301-072354.jpg"
-                },
-                {
-                    id: 2,
-                    titulo: "Pizza Napoletana ai carciofi",
-                    costo: 675.00,
-                    portada: "https://static.cookist.it/wp-content/uploads/sites/21/2017/12/istock-480277738.jpg"
-                },
-                {
-                    id: 3,
-                    titulo: "Porchetta umbra a cottura lunga",
-                    costo: 845.00,
-                    portada: "https://www.fontecesia.it/wp-content/uploads/2018/11/porchetta-umbra-1920x1280.jpg"
-                },
-                {
-                    id: 4,
-                    titulo: "Orecchiette alle cime di rapa",
-                    costo: 845.00,
-                    portada: "https://irepo.primecp.com/2016/03/259860/recipe-8673_ExtraLarge1000_ID-1461628.jpg"
-                }
-            ]
 
         }
     }
